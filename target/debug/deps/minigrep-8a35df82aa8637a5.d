bc82ec415e1efd4fccee028eff79d7df307d3ca8
come-bianchi <come.bianchi@schibsted.com>
2025-05-01 16:04:25 +0200

    scaffolding

---
+++ target/debug/deps/minigrep-8a35df82aa8637a5.d
@@ -0,0 +1,5 @@
+C:\Users\AAD947\Desktop\code\rust\minigrep\target\debug\deps\libminigrep-8a35df82aa8637a5.rmeta: src\main.rs
+
+C:\Users\AAD947\Desktop\code\rust\minigrep\target\debug\deps\minigrep-8a35df82aa8637a5.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/minigrep-dc47a46f18a88f7c.d
@@ -0,0 +1,5 @@
+C:\Users\AAD947\Desktop\code\rust\minigrep\target\debug\deps\libminigrep-dc47a46f18a88f7c.rmeta: src\main.rs
+
+C:\Users\AAD947\Desktop\code\rust\minigrep\target\debug\deps\minigrep-dc47a46f18a88f7c.d: src\main.rs
+
+src\main.rs:
